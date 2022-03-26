aa45b2e52f349f02a5cb323cb5ad8f3220d22cf3
Tarequl Islam <tareque.dev@gmail.com>
2022-03-26 17:44:30 +0600

    favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Shop from './components/shop/Shop';
-import QuestionAnswer from './components/QuestionAnswer';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Shop from "./components/shop/Shop";
+import QuestionAnswer from "./components/QuestionAnswer";
 
 function App() {
   return (
